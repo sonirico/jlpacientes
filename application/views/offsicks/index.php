38cf9627ba895252dbec0b87e25c583781f8be38
Marcos <marcos.sanchez@a3sec.com>
2017-09-12 20:28:19 +0200

    Add filters to offsick table

--- application/views/offsicks/index.php
+++ application/views/offsicks/index.php
@@ -1,19 +1,51 @@
 <?php $this->load->view('partials/head', ['extra_css' => [
-    'assets/css/offsicks/index.css'
+    'assets/css/offsicks/index.css',
+    '/assets/lib/bootstrap-datepicker/css/bootstrap-datepicker.standalone.min.css'
 ]]); ?>
 
 <?php $this->load->view('partials/navbar'); ?>
 
 <div class="container-fluid" id="offsicks-container" >
+    <form class="filter-form" id="offsicks-form-filter">
+        <div class="row" >
+            <div class="col-md-4" >
+                <div class="form-group" >
+                    <div class="checkbox" >
+                        <label>
+                            <input type="checkbox" name="offsicks-filter-show-ups" >
+                            Mostrar altas
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4" >
+                <div class="form-group" >
+                    <div class='input-group date' id='datetimepicker9'>
+                        <span class="input-group-addon">
+                            Desde
+                        </span>
+                        <input type='text' name="offsicks-filter-date-from" class="form-control" />
+                        <span class="input-group-addon">
+                            Hasta
+                        </span>
+                        <input type='text' name="offsicks-filter-date-to" class="form-control" />
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4" >
+                <div class="form-group text-right" >
+                    <a target="_blank" class="btn btn-default" role="button"
+                       href="<?php echo base_url('/offsicks/export'); ?>" >
+                        <i class="fa fa-file-pdf-o" >&nbsp;</i>
+                        Exportar
+                    </a>
+                </div>
+            </div>
+        </div>
+    </form>
     <div class="row" >
         <div class="col-lg-12" >
-            <div class="form-group text-right" >
-                <a target="_blank" class="btn btn-default" role="button"
-                    href="<?php echo base_url('/offsicks/export'); ?>" >
-                    <i class="fa fa-file-pdf-o" >&nbsp;</i>
-                    Exportar
-                </a>
-            </div>
             <div class="table-responsive" >
                 <table id="offsicks-table" class="table table-bordered"  >
                     <tbody>
@@ -36,5 +68,8 @@
 </script>
 
 <?php $this->load->view('partials/footer', ['extra_js' => [
+    '/assets/lib/bootstrap-datepicker/js/bootstrap-datepicker.min.js',
+    '/assets/lib/bootstrap-datepicker/locales/bootstrap-datepicker.es.min.js',
+    'assets/lib/momentjs/moment.js',
     'assets/js/offsicks/index.js'
 ]]); ?>
--- assets/js/offsicks/index.js
+++ assets/js/offsicks/index.js
@@ -1,145 +1,234 @@
+$(function () {
+    offsicks = (function () {
 
-offsicks = (function () {
+        var tableObj = {};
+        var filters = {
+            toggleUps: $('[name="offsicks-filter-show-ups"]'),
+            dateFrom: $('[name="offsicks-filter-date-from"]'),
+            dateTo: $('[name="offsicks-filter-date-to"]')
+        };
 
-    var tableObj = {};
-
-    var loadTable = function (data) {
-        return new Promise(function (resolve, reject) {
-            tableObj = $('#offsicks-table').DataTable({
-                'processing': true,
-                'deferRender': true,
-                'pageLength': 50,
-                'lengthMenu': [5, 10, 20, 50, 100],
-                'order': [
-                    [0, 'desc']
-                ],
-                'columns': [
-                    {
-                        'title': 'Fecha baja',
-                        'data': 'created_at',
-                        'className': 'offsick-created-at',
-                        'visible': true,
-                        'orderable': true
-                    },
-                    {
-                        'title': 'Fecha alta',
-                        'data': 'ended_at',
-                        'className': 'offsick-ended-at',
-                        'visible': true,
-                        'orderable': true,
-                        'render': function (data, type, row, meta) {
-                            if ('display' === type) {
-                                if (data) return data;
-                                return '-';
-                            }
-
-                            return data;
-                        }
-                    },
-                    {
-                        'title': 'Jugador',
-                        'data': 'player_full_name',
-                        'className': 'patient-name',
-                        'visible': true,
-                        'orderable': true,
-                        'render': function (data, type, row, meta) {
-                            if ('display' === type)
-                            {
-                                var link = document.createElement('a');
-
-                                link.href = "/players/" + row.player_id + '/show/';
-                                link.target = "_blank";
-                                link.title = "Ver perfil";
-                                link.textContent = data;
-
-                                return link.outerHTML;
-                            }
-
-                            return data;
-                        }
-                    },
-                    {
-                        'title': 'Fase',
-                        'data': 'current_stage',
-                        'className': 'patient-stage',
-                        'visible': true,
-                        'orderable': true,
-                        'render': function (data, type, row, meta) {
-                            if ('display' === type) return (stages[data] || 'N/A');
-                            return data;
-                        }
-                    },
-                    // {
-                    //     'title': 'Lesión asociada/',
-                    //     'data': 'injury',
-                    //     'className': 'patient-injury',
-                    //     'visible': true,
-                    //     'orderable': true,
-                    //     'render': function (data, type, row, meta) {
-                    //         if ('display' === type || 'filter' === type) return injuries[data];
-                    //         return data;
-                    //     }
-                    // },
-                    // {
-                    //     'title': 'Acciones',
-                    //     'data': null,
-                    //     'className': 'patient-actions',
-                    //     'visible': true,
-                    //     'orderable': false,
-                    //     'createdCell': function (cell, rowData, row, col) {
-                    //         // $(cell).html(
-                    //         //     utils.template('#button-container')
-                    //         // );
-                    //     }
-                    // }
-                ],
-                'data': data,
-                'initComplete': function () {
-                    resolve();
-                },
-                'language': {
-                    'url': DT_LANGUAGE_URL,
-                }
-            });
-        });
-    };
-
-    var loadData = function () {
-        return $.ajax({
-            'url': OFFSICKS_URL,
-            'method': 'GET',
-            'dataType': 'json'
-        });
-    }
-
-    return {
-        'init': function () {
+        var loadTable = function (data) {
             return new Promise(function (resolve, reject) {
+                tableObj = $('#offsicks-table').DataTable({
+                    'processing': true,
+                    'deferRender': true,
+                    'pageLength': 50,
+                    'lengthMenu': [5, 10, 20, 50, 100],
+                    'order': [
+                        [0, 'desc']
+                    ],
+                    'columns': [
+                        {
+                            'title': 'Fecha baja',
+                            'data': 'created_at',
+                            'className': 'offsick-created-at',
+                            'visible': true,
+                            'orderable': true
+                        },
+                        {
+                            'title': 'Fecha alta',
+                            'data': 'ended_at',
+                            'className': 'offsick-ended-at',
+                            'visible': true,
+                            'orderable': true,
+                            'render': function (data, type, row, meta) {
+                                if ('display' === type) {
+                                    if (data) return data;
+                                    return '-';
+                                }
 
-                loadData().done(function (data) {
+                                return data;
+                            }
+                        },
+                        {
+                            'title': 'Jugador',
+                            'data': 'player_full_name',
+                            'className': 'patient-name',
+                            'visible': true,
+                            'orderable': true,
+                            'render': function (data, type, row, meta) {
+                                if ('display' === type)
+                                {
+                                    var link = document.createElement('a');
 
-                    loadTable(data).then(function (data) {
-                        console.log(data);
-                        console.log('loaded!');
+                                    link.href = "/players/" + row.player_id + '/show/';
+                                    link.target = "_blank";
+                                    link.title = "Ver perfil";
+                                    link.textContent = data;
 
+                                    return link.outerHTML;
+                                }
+
+                                return data;
+                            }
+                        },
+                        {
+                            'title': 'Fase',
+                            'data': 'current_stage',
+                            'className': 'patient-stage',
+                            'visible': true,
+                            'orderable': true,
+                            'render': function (data, type, row, meta) {
+                                if ('display' === type) return (stages[data] || 'N/A');
+                                return data;
+                            }
+                        },
+                        // {
+                        //     'title': 'Lesión asociada/',
+                        //     'data': 'injury',
+                        //     'className': 'patient-injury',
+                        //     'visible': true,
+                        //     'orderable': true,
+                        //     'render': function (data, type, row, meta) {
+                        //         if ('display' === type || 'filter' === type) return injuries[data];
+                        //         return data;
+                        //     }
+                        // },
+                        // {
+                        //     'title': 'Acciones',
+                        //     'data': null,
+                        //     'className': 'patient-actions',
+                        //     'visible': true,
+                        //     'orderable': false,
+                        //     'createdCell': function (cell, rowData, row, col) {
+                        //         // $(cell).html(
+                        //         //     utils.template('#button-container')
+                        //         // );
+                        //     }
+                        // }
+                    ],
+                    'data': data,
+                    'initComplete': function () {
                         resolve();
-
-                    }).catch(function (err) {
-                        console.log(err);
-                    });
-
-                }).fail(function (jqXHR, textStatus) {
-                    console.error(textStatus);
+                    },
+                    'language': {
+                        'url': DT_LANGUAGE_URL,
+                    }
                 });
             });
-        },
-        'events': function () {
+        };
 
-        }
-    };
-})();
+        var loadData = function () {
+            return $.ajax({
+                'url': OFFSICKS_URL,
+                'method': 'GET',
+                'dataType': 'json'
+            });
+        };
 
 
-offsicks.init().then(function () {
-    offsicks.events();
+        var loadFilters = function (){
+            // Toggle ups/downs
+            $.fn.dataTable.ext.search.push(
+                function( settings, data, dataIndex ) {
+                    var hasUp = data[1].trim().length > 0;
+
+                    if (! hasUp) return true;
+
+                    return filters.toggleUps.is(':checked');
+                }
+            );
+
+            // Greater or equals than from
+            $.fn.dataTable.ext.search.push(
+                function( settings, data, dataIndex ) {
+
+                    var dateFrom = filters.dateFrom.val().trim();
+
+                    if (dateFrom.length < 1) return true;
+
+                    var from = moment(dateFrom, 'DD/MM/YYYY');
+                    var happened = moment(data[0], 'YYYY-MM-DD');
+
+                    return from <= happened;
+                }
+            );
+
+            // Lower or equals than to
+            $.fn.dataTable.ext.search.push(
+                function( settings, data, dataIndex ) {
+                    var dateTo = filters.dateTo.val().trim();
+
+                    if (dateTo.length < 1) return true;
+
+                    var to = moment(dateTo, 'DD/MM/YYYY');
+                    var happened = moment(data[0], 'YYYY-MM-DD');
+
+                    return happened <= to;
+                }
+            );
+        };
+
+        return {
+            'init': function () {
+                return new Promise(function (resolve, reject) {
+
+                    loadData().done(function (data) {
+
+                        loadTable(data).then(function (data) {
+
+                            loadFilters();
+
+                            resolve();
+
+                        }).catch(function (err) {
+                            console.log(err);
+                        });
+
+                    }).fail(function (jqXHR, textStatus) {
+                        console.error(textStatus);
+                    });
+                });
+            },
+            'events': function () {
+                filters.toggleUps.click(function (e) {
+                    tableObj.draw();
+                });
+
+                filters.dateFrom.datepicker({
+                    format: "dd/mm/yyyy",
+                    todayBtn: "linked",
+                    clearBtn: true,
+                    language: "es",
+                    autoclose: true,
+                    todayHighlight: true
+                });
+
+                filters.dateTo.datepicker({
+                    format: "dd/mm/yyyy",
+                    todayBtn: "linked",
+                    clearBtn: true,
+                    language: "es",
+                    autoclose: true,
+                    todayHighlight: true
+                });
+
+                filters.dateFrom.change(function (e) {
+                    tableObj.draw();
+                });
+
+                filters.dateTo.change(function (e) {
+                    tableObj.draw();
+                });
+            },
+            'tableDraw': function () {
+                tableObj.draw();
+            }
+        };
+    })();
+
+
+    offsicks.init().then(function () {
+        offsicks.events();
+
+        setTimeout(function () {
+
+            // Hide ups
+            offsicks.tableDraw();
+
+
+        }, 500);
+    });
+
 });
